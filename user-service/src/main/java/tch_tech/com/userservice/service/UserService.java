94e5986ba317470b46a2841e6096b93bc00aa3bd
Marc BILICK BASSI <bilickbassi@gmail.com>
2025-02-20 23:22:38 +0100

    Updates

--- user-service/src/main/java/tch_tech/com/userservice/service/UserService.java
+++ user-service/src/main/java/tch_tech/com/userservice/service/UserService.java
@@ -1,12 +1,12 @@
 package tch_tech.com.userservice.service;
 
 import lombok.RequiredArgsConstructor;
-import org.hibernate.sql.Update;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import tch_tech.com.userservice.model.UserEntity;
+import tch_tech.com.userservice.model.UserRole;
 import tch_tech.com.userservice.repository.UserRepository;
+import tch_tech.com.userservice.repository.UserRoleRespository;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,19 +15,54 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserServiceInterface {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserRoleRespository userRoleRepository;
+
+    @Override
+    public UserEntity addNewUser(UserEntity userEntity) {
+        if (userRepository.existsByEmail(userEntity.getEmail())) {
+            throw new RuntimeException("User with email " + userEntity.getEmail() + " already exists");
+        }
+        userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
+        return userRepository.save(userEntity);
+    }
+
+    @Override
+    public UserRole addNewRole(UserRole userRole) {
+        return userRoleRepository.save(userRole);
+    }
+
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        UserEntity user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        UserRole role = userRoleRepository.findByRoleName(roleName)
+                .orElseThrow(() -> new RuntimeException("Role not found"));
+        user.getRoles().add(role);
+        userRepository.save(user);
+    }
+
+
+    @Override
+    public UserEntity loadUserByUsername(String username) {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
 
 
     public List<UserEntity> getAllUsers() {
         return userRepository.findAll();
     }
 
+    @Override
     public Optional<UserEntity> getUserById(Long id) {
         return userRepository.findById(id);
     }
 
+    @Override
     public Optional<UserEntity> updateUser(Long id, UserEntity userEntity) {
         return userRepository.findById(id).map(
                 existingUser -> {
@@ -40,11 +75,7 @@ public class UserService {
         );
     }
 
-    public String deleteUserById(Long id) {
-        userRepository.deleteById(id);
-        return "User deleted successfully";
-    }
-
+    // Save user
     public UserEntity saveUser(UserEntity userEntity) {
         if (userRepository.existsByEmail(userEntity.getEmail())) {
             throw new RuntimeException(
@@ -54,5 +85,11 @@ public class UserService {
         userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
         return userRepository.save(userEntity);
     }
+
+    // delete user
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+
 
 }
