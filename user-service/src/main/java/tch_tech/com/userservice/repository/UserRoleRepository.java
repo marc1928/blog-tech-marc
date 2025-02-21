e6b30082ab13ac1609c66eba92ddb8371fd21dfa
Marc BILICK BASSI <bilickbassi@gmail.com>
2025-02-21 15:12:17 +0100

    User service v1 OK

--- user-service/src/main/java/tch_tech/com/userservice/repository/UserRoleRepository.java
+++ user-service/src/main/java/tch_tech/com/userservice/repository/UserRoleRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tch_tech.com.userservice.model.UserRole;
 
 
-public interface UserRoleRespository extends JpaRepository<UserRole, Long> {
+public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
     UserRole findByRoleName(String roleName);
 }
